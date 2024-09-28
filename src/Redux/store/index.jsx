db5817ab14f78eb5c58bf94d94ab0174831ba1a3
Shravan10 <shravankumarghanchi@gmail.com>
2024-09-28 23:49:13 +0530

    create cart slice and import in store

--- src/Redux/store/index.jsx
+++ src/Redux/store/index.jsx
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 // import counterSlice from '../counterSlice';
 import counterSlice from '../../counterSlice';
-// import cartSlice from '../cartSlice';
+import cartSlice from '../../cartSlice';
 export const store = configureStore({
   reducer: {
     counterShravan: counterSlice,
-    // cart: cartSlice,
+    cart: cartSlice,
   },
 });
