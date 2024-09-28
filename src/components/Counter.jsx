c54487bdb7111238427884d93a7933692fbe1f24
Shravan10 <shravankumarghanchi@gmail.com>
2024-09-28 21:56:30 +0530

    added function by name

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -8,7 +8,7 @@ return(
     <div>
         <h1>React Redux Demo Project</h1>
    <h1>{count}</h1>
-   <button onClick={()=>{dispatch(increaseBy1())}}>Increased by one</button>
+   <button onClick={()=>{dispatch(increaseBy1())}}>Increased by 1</button>
    <button onClick={()=>{dispatch(increaseBy10())}}>Increased by 10</button>
    <button onClick={()=>{dispatch(decreasedBy1())}}> Decresased by 1</button>
    <button onClick={()=>{dispatch(decreasedBy10())}}>Decreased by 10</button>
